2a1f35d6db1a02b285153fc76c18ba4582d53840
taseeersam <68563974+taseeersam@users.noreply.github.com>
2025-02-04 17:31:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('yesButton').addEventListener('click', function() {
     document.getElementById('response').textContent = 'Yipeeeeeeeeeeeeee!';
 });
+
+document.getElementById('noButton').addEventListener('click', function() {
+    document.getElementById('response').textContent = 'No means yes, so WOOPPEEEEEEE!';
+});
