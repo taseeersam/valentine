fa9eee573aa822f718c3ca23beef51a4dff64bd1
taseeersam <68563974+taseeersam@users.noreply.github.com>
2025-02-04 17:27:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('yesButton').addEventListener('click', function() {
-    document.getElementById('response').textContent = 'Yay! I can\'t wait to see you!';
+    document.getElementById('response').textContent = 'Yipeeeeeeeeeeeeee!';
 });
